1d62cb1e13d74b043f5dd9053d825ab48a9cab78
Krzysztof Bojarczuk <krzysztofbojarczuk@wp.pl>
2024-01-01 07:30:51 +0100

    Given an array of integers.
    Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
    
    If the input is an empty array or is null, return an empty array.
    
    Example
    For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].

---
+++ Codewars/ArrayProcessor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    public class ArrayProcessor
+    {
+        public int[] CountPositivesSumNegatives(int[] arr)
+        {
+            //if (arr == null || arr.Length == 0)
+            //{
+            //    return new int[0];
+            //}
+
+            //int countPositives = 0;
+            //int sumNegatives = 0;
+
+            //foreach (int num in arr)
+            //{
+            //    if (num > 0)
+            //    {
+            //        countPositives++;
+            //    }
+            //    else if (num < 0)
+            //    {
+            //        sumNegatives += num;
+            //    }
+            //}
+
+            //return new int[] { countPositives, sumNegatives };
+
+            if (arr == null || arr.Length == 0)
+            {
+                return new int[0];
+            }
+
+            int countPositives = arr.Count(num => num > 0);
+            int sumNegatives = arr.Where(num => num < 0).Sum();
+
+            return new int[] { countPositives, sumNegatives };
+        }
+    }
+}
