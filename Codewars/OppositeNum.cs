eed0d7404610091df0513f37494baeabec4f892b
Krzysztof Bojarczuk <krzysztofbojarczuk@wp.pl>
2024-02-23 08:44:54 +0100

    given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).

---
+++ Codewars/OppositeNum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    public class OppositeNum
+    {
+        public static int GetOpposite(int number)
+        {
+            return -number;
+        }
+
+        public static int MakePositive(int number)
+        {
+            return Math.Abs(number);
+        }
+    }
+}
