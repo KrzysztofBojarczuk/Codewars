c02045080cbffdbf3af61953722a1a8981ae1537
Krzysztof Bojarczuk <krzysztofbojarczuk@wp.pl>
2024-02-26 07:06:58 +0100

    format for expressing an ordered list of integers is to use a comma separated list of either

---
+++ Codewars/RangeExtraction.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    using System.Text;
+
+    public class RangeExtraction
+    {
+        public string Extract(int[] args)
+        {
+            if (args == null || args.Length == 0)
+                return "";
+
+            StringBuilder result = new StringBuilder();
+            int startRange = args[0];
+            int endRange = args[0];
+
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (args[i] == endRange + 1)
+                {
+                    endRange = args[i];
+                }
+                else
+                {
+                    if (startRange == endRange)
+                    {
+                        result.Append(startRange).Append(",");
+                    }
+                    else if (endRange - startRange == 1)
+                    {
+                        result.Append(startRange).Append(",").Append(endRange).Append(",");
+                    }
+                    else
+                    {
+                        result.Append(startRange).Append("-").Append(endRange).Append(",");
+                    }
+
+                    startRange = endRange = args[i];
+                }
+            }
+
+            // Handle the last range
+            if (startRange == endRange)
+            {
+                result.Append(startRange);
+            }
+            else if (endRange - startRange == 1)
+            {
+                result.Append(startRange).Append(",").Append(endRange);
+            }
+            else
+            {
+                result.Append(startRange).Append("-").Append(endRange);
+            }
+
+            return result.ToString();
+        }
+    }
+}
